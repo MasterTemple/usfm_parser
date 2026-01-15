44e7007dba18d67ea587ca8176b45a4e0a9fe934
Blake Scampone <blake.scampone@gmail.com>
2026-01-14 17:00:21 -0800

    Assigned tag

--- src/markers/markers/mod.rs
+++ src/markers/markers/mod.rs
@@ -1,9 +1,15 @@
 pub mod all;
 pub mod any;
+// `\va` and `\va`
 pub mod paired;
 pub mod parameters;
+// `\v`
 pub mod simple;
+// `\q1`
 pub mod weighted;
+
+// `\alt=`
+pub mod assigned;
 
 pub mod parser;
 pub mod tag;
